8c2f2646cc00968731041f75327141240d152f35
Михаил <mmamchenkov@fil-it.ru>
2022-02-02 12:51:22 +0300

    Добавление теста

---
+++ src/test/java/mobile/makeorder/unauthorized/SelfPickupOrderingTest.java
@@ -0,0 +1,41 @@
+package mobile.makeorder.unauthorized;
+
+import base.BaseSettingsMobileTests;
+import io.qameta.allure.Feature;
+import io.qameta.allure.Story;
+import io.qameta.allure.junit4.DisplayName;
+import org.junit.Test;
+
+@Feature(value = "Web")
+@Story(value = "оформление заказа самовывозом")
+@DisplayName("Оформление заказа самовывозом")
+public class SelfPickupOrderingTest extends BaseSettingsMobileTests {
+
+    @DisplayName("Неавторизованный пользователь покупает товар со способом доставки - 'Самовывоз'")
+    @Test
+    public void pickup() {
+        mobileMainPage.clickClosePopUp();
+        mobileMainPage.clickCloseMobileAppPopUp();
+        pageActions.waitPageLoad();
+        mobileMainPage.setSearchInput(propertiesManager.getProperty("productcode1"));
+        mobileCartPage.сlickAddCartButton();
+        pageActions.waitPageLoad();
+        mobileCartPage.clickToCartButton();
+        mobileCartPage.clickToMakeOrder();
+        mobileCheckOutPage.clickSelectApteka();
+        mobileProductCardPage.checkVisibilityMap();
+        mobileCheckOutPage.setInputSearchAddres("метро Фили");
+        pageActions.waitPageLoad();
+        mobileCheckOutPage.clickChangeAptekaList();
+        mobileCheckOutPage.ckickSelect();
+        pageActions.waitPageLoad();
+        mobileCheckOutPage.contactDetails(
+                propertiesManager.getProperty("username"),
+                propertiesManager.getProperty("phonenumber"),
+                propertiesManager.getProperty("usermail"));
+        mobileCheckOutPage.clickMakeOrder();
+        pageActions.waitPageLoad();
+        mobileThankForTheOrderPage.checkSuccessMessage();
+    }
+
+}
