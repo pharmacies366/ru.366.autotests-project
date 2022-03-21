2f2e9d66d194107e06c728b852c10723059ce95c
Михаил <mmamchenkov@fil-it.ru>
2022-03-21 13:52:06 +0300

    Рефакторинг репы, удаление api тестов из репозитория

--- src/main/java/actions/PageElementActions.java
+++ src/main/java/actions/PageElementActions.java
@@ -7,7 +7,7 @@ import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
-import static org.hamcrest.Matchers.not;
+import static org.hamcrest.core.IsNot.not;
 
 
 public class PageElementActions extends MainTestBase {
