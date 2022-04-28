4c36760d61272fcdf8207a6c36309fd941a07f81
Михаил <mmamchenkov@fil-it.ru>
2022-04-28 19:18:58 +0300

    тестовый рефакторинг кода

--- src/main/java/utils/WebDriverFactory.java
+++ src/main/java/utils/WebDriverFactory.java
@@ -130,8 +130,8 @@ public class WebDriverFactory {
     private void configureDriver() {
         driver.manage().deleteAllCookies();
         driver.manage().window().maximize();
+        driver.manage().timeouts().pageLoadTimeout(10, TimeUnit.SECONDS);
         driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
-        driver.manage().timeouts().pageLoadTimeout(15, TimeUnit.SECONDS);
     }
 
 
