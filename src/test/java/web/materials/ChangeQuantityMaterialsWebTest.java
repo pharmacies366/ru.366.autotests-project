489b89b2a480b50697f1673f38f23b928d0fdcab
Михаил <mmamchenkov@fil-it.ru>
2022-05-27 14:45:48 +0300

    Перенос тестов

---
+++ src/test/java/web/materials/ChangeQuantityMaterialsWebTest.java
@@ -0,0 +1,72 @@
+package web.materials;
+
+import base.BaseSettingsWebTests;
+import io.qameta.allure.Feature;
+import io.qameta.allure.Story;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+
+@Feature(value = "Web")
+@Story(value = "Изменение количества товара в корине на сайте 36.6")
+@DisplayName("Изменение количества товара в корзине")
+public class ChangeQuantityMaterialsWebTest extends BaseSettingsWebTests {
+
+    @DisplayName("Пользователь увеличивает количество шт. товара в корзине на главной странице")
+    @Test
+    public void increaseQuantityMaterialOnMainPage() {
+        mainPage.clickClosePopUp();
+        mainPage.ClickClosePopUpNewsButton();
+        mainPage.clickAddToCardButton();
+        pageActions.waitPageLoad();
+        mainPage.clickIncreaseQuantity();
+        int firstQuantity = mainPage.getQuantityMaterials();
+        Assertions.assertEquals(2, firstQuantity);
+        logger.info("Количество товара равно 2-м");
+        headerBlock.clickToCartButton();
+        int finalQuantity = cartPage.getQuantityMaterials();
+        Assertions.assertEquals(2, finalQuantity);
+        logger.info("Количество товара равно 2-м");
+    }
+
+    @DisplayName("Пользователь уменьшает количество шт. товара в корзине на главной странице")
+    @Test
+    public void decreaseQuantityMaterialOnMainPage() {
+        mainPage.clickClosePopUp();
+        mainPage.ClickClosePopUpNewsButton();
+        mainPage.clickAddToCardButton();
+        pageActions.waitPageLoad();
+        mainPage.clickIncreaseQuantity();
+        int firstQuantity = mainPage.getQuantityMaterials();
+        Assertions.assertEquals(2, firstQuantity);
+        logger.info("Количество товара равно 2-м");
+        mainPage.clickDecreaseQuantity();
+        int secondQuantity = mainPage.getQuantityMaterials();
+        Assertions.assertEquals(1, secondQuantity);
+        logger.info("Количество товара равно 1-му");
+        headerBlock.clickToCartButton();
+        int finalQuantity = cartPage.getQuantityMaterials();
+        Assertions.assertEquals(1, finalQuantity);
+        logger.info("Количество товара равно 1-му");
+    }
+
+    @DisplayName("Пользователь увеличивает количество шт. товара в карточке товара")
+    @Test
+    public void increaseQuantityMaterialOnProductCartPage() {
+        mainPage.clickClosePopUp();
+        openUrl(propertiesManager.getProperty("baseurl") + "p/29992");
+        productCardPage.clickAddCartButton();
+        pageActions.waitPageLoad();
+        productCardPage.clickIncreaseQuantity();
+        int quantity = productCardPage.getQuantityProduct();
+        Assertions.assertEquals(2, quantity);
+        logger.info("Количество товара равно 2-м");
+        productCardPage.clickToCartButton();
+        int finalQuantity = cartPage.getQuantityMaterials();
+        Assertions.assertEquals(2, finalQuantity);
+        logger.info("Количество товара равно 2-м");
+
+    }
+
+}
