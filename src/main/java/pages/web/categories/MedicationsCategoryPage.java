a9165c82fb05dfc72243ea66ec43849a0a52bc2c
Михаил <mmamchenkov@fil-it.ru>
2022-06-01 18:07:52 +0300

    migration to junit5

---
+++ src/main/java/pages/web/categories/MedicationsCategoryPage.java
@@ -0,0 +1,23 @@
+package pages.web.categories;
+
+import core.MainTestBase;
+import org.openqa.selenium.WebDriver;
+
+public class MedicationsCategoryPage extends MainTestBase {
+
+
+    //элементы
+
+
+    //конструктор
+    public MedicationsCategoryPage(WebDriver driver) {
+        this.driver = driver;
+    }
+
+
+    //геттеры элементов с получением доступа к действиям с элементами
+
+    //Методы
+
+
+}
