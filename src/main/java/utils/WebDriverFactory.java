57ce26b1d323defbc06d1be8ecdc06c20ecdf9c2
Михаил <mmamchenkov@fil-it.ru>
2022-06-01 19:56:40 +0300

    migration to junit5

--- src/main/java/utils/WebDriverFactory.java
+++ src/main/java/utils/WebDriverFactory.java
@@ -13,6 +13,7 @@ import org.openqa.selenium.remote.RemoteWebDriver;
 
 import java.net.MalformedURLException;
 import java.net.URI;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.concurrent.TimeUnit;
@@ -21,6 +22,7 @@ import static core.MainTestBase.nameOfPackage;
 
 
 public class WebDriverFactory {
+
     private Logger logger = LogManager.getLogger(WebDriverFactory.class);
     private RemoteWebDriver driver;
 
@@ -41,21 +43,36 @@ public class WebDriverFactory {
     @Step("Настройка удаленного драйвера")
     private void setupRemoteDriver() {
         logger.info("setup remote driver");
-        String driverURL = System.getProperty("driverurl");
+        ChromeOptions chromeOptions = new ChromeOptions();
         DesiredCapabilities capabilities = new DesiredCapabilities();
-        capabilities.setBrowserName("chrome");
-        capabilities.setVersion("91.0");
+        chromeOptions.addArguments("--incognito");
+        chromeOptions.addArguments("--disable-notifications");
+        chromeOptions.addArguments("--disable-extensions");
+        chromeOptions.addArguments("--dns-prefetch-disable");
+        Map<String, Object> prefs = new HashMap<String, Object>();
+        Map<String, Object> profile = new HashMap<String, Object>();
+        prefs.put("googlegeolocationaccess.enabled", true);
+        prefs.put("profile.default_content_setting_values.geolocation", 2); // 1:allow 2:block
+        prefs.put("profile.default_content_setting_values.notifications", 1);
+        prefs.put("profile.managed_default_content_settings", 1);
+        chromeOptions.setExperimentalOption("prefs", prefs);
+        chromeOptions.setPageLoadStrategy(PageLoadStrategy.NORMAL);
         capabilities.setCapability("enableVNC", true);
         capabilities.setCapability("enableVideo", false);
+
+
         if (nameOfPackage.contains("mobile")) {
             Map<String, String> mobileEmulation = new HashMap<>();
             mobileEmulation.put("deviceName", "iPhone X");
-
-            ChromeOptions chromeOptions = new ChromeOptions();
             chromeOptions.setExperimentalOption("mobileEmulation", mobileEmulation);
-            chromeOptions.setPageLoadStrategy(PageLoadStrategy.NORMAL);
             capabilities.setCapability(ChromeOptions.CAPABILITY, chromeOptions);
         }
+
+
+        capabilities.setCapability(ChromeOptions.CAPABILITY, chromeOptions);
+        String driverURL = System.getProperty("driverurl");
+        capabilities.setBrowserName("chrome");
+        capabilities.setVersion("91.0");
 
         try {
             driver = new RemoteWebDriver(
@@ -65,34 +82,49 @@ public class WebDriverFactory {
         } catch (MalformedURLException e) {
             e.printStackTrace();
         }
+
         configureDriver();
         logger.info("ЗАПУЩЕН УДАЛЕННЫЙ ДРАЙВЕР");
     }
 
+
     @Step("Настройка локального драйвера")
     public void setupLocalDriver() {
-        logger.info("setup local web driver");
+        logger.info("setup local driver");
         ChromeOptions chromeOptions = new ChromeOptions();
         DesiredCapabilities capabilities = new DesiredCapabilities();
+        //DesiredCapabilities capabilities = DesiredCapabilities.chrome(); //в чем разница?
+
+
         chromeOptions.addArguments("--incognito");
+        chromeOptions.setExperimentalOption("excludeSwitches", Collections.singletonList("enable-automation"));//оставить
         chromeOptions.addArguments("--disable-notifications");
+        Map<String, Object> prefs = new HashMap<String, Object>();
+        Map<String, Object> profile = new HashMap<String, Object>();
+        prefs.put("googlegeolocationaccess.enabled", true);
+        prefs.put("profile.default_content_setting_values.geolocation", 2); // 1:allow 2:block
+        prefs.put("profile.default_content_setting_values.notifications", 1);
+        prefs.put("profile.managed_default_content_settings", 1);
+        chromeOptions.setExperimentalOption("prefs", prefs);
+        chromeOptions.setPageLoadStrategy(PageLoadStrategy.NORMAL);
+        chromeOptions.addArguments("--ignore-certificate-errors");
+        chromeOptions.addArguments("--disabled-notifications");
+        chromeOptions.addArguments("--disabled-popup-blocking");
+        chromeOptions.addArguments("--disable-extensions");
+        chromeOptions.addArguments("--dns-prefetch-disable");
+
+
         if (nameOfPackage.contains("mobile")) {
             WebDriverManager.chromedriver().setup();
             Map<String, String> mobileEmulation = new HashMap<>();
             mobileEmulation.put("deviceName", "iPhone X");
-            Map<String, Object> prefs = new HashMap<String, Object>();
-            Map<String, Object> profile = new HashMap<String, Object>();
-            prefs.put("googlegeolocationaccess.enabled", true);
-            prefs.put("profile.default_content_setting_values.geolocation", 2); // 1:allow 2:block
-            prefs.put("profile.default_content_setting_values.notifications", 1);
-            prefs.put("profile.managed_default_content_settings", 1);
-            chromeOptions.setExperimentalOption("prefs", prefs);
             chromeOptions.setExperimentalOption("mobileEmulation", mobileEmulation);
-            capabilities.setCapability(ChromeOptions.CAPABILITY, chromeOptions);
-            chromeOptions.setPageLoadStrategy(PageLoadStrategy.NORMAL);
         }
+
+        capabilities.setCapability(ChromeOptions.CAPABILITY, chromeOptions);
         WebDriverManager.chromedriver().setup();
         driver = new ChromeDriver(capabilities);
+        //driver = new ChromeDriver(chromeOptions);
         configureDriver();
         logger.info("ЗАПУЩЕН ЛОКАЛЬНЫЙ ДРАЙВЕР");
     }
@@ -100,6 +132,7 @@ public class WebDriverFactory {
 
     @Step("Конфигурация драйвера")
     private void configureDriver() {
+        driver.manage().deleteAllCookies();
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
     }
