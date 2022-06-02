c4bdbf544aa056da4bba227433bb124021addadd
Михаил <mmamchenkov@fil-it.ru>
2022-06-02 21:41:04 +0300

    migration to junit5

--- src/test/java/mobile/mainPage/MobileHeaderBlockMobileTest.java
+++ src/test/java/mobile/mainPage/MobileHeaderBlockMobileTest.java
@@ -26,7 +26,7 @@ public class MobileHeaderBlockMobileTest extends BaseSettingsMobileTests {
     @Test
     public void testingTheSearch(){
         mobileHeaderBlock.setSearchInput(propertiesManager.getProperty("productcode4"));
-        pageActions.checkUrl("https://366.cwzw6gg24a-llcapteka1-p1-public.model-t.cc.commerce.ondemand.com/search/?text=44226");
+        pageActions.checkUrl("https://366.cwzw6gg24a-llcapteka1-s1-public.model-t.cc.commerce.ondemand.com/search/?text=35523");
         logger.info("Поиск работает корректно");
     }
 
