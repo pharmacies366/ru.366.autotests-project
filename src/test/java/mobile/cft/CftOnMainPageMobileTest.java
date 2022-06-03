d98993651092afa186f36729f1ac33cc77c65682
Михаил <mmamchenkov@fil-it.ru>
2022-06-03 17:03:31 +0300

    migration to junit5

--- src/test/java/mobile/cft/CftOnMainPageMobileTest.java
+++ src/test/java/mobile/cft/CftOnMainPageMobileTest.java
@@ -18,6 +18,7 @@ import org.junit.jupiter.api.Test;
 @Story("ЦФТ на сайте 36.6")
 @DisplayName("Проверка работы ЦФТ на главной странице сайта 36.6")
 @Tag("Regression")
+@Tag("CFT")
 public class CftOnMainPageMobileTest extends BaseSettingsMobileTests {
 
     @DisplayName("Проверка заднего фона кнопки с расчётом бонусов")
