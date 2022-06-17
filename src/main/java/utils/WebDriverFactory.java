2ffe76b526cbebe6aa881180adb32e4c17767fae
Михаил <mmamchenkov@fil-it.ru>
2022-06-17 17:22:14 +0300

    Добавлены правки для стабильности тестов

--- src/main/java/utils/WebDriverFactory.java
+++ src/main/java/utils/WebDriverFactory.java
@@ -18,12 +18,12 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.concurrent.TimeUnit;
 
-import static core.MainTestBase.nameOfClass;
 import static core.MainTestBase.nameOfPackage;
+
 
 public class WebDriverFactory {
 
-    private final Logger logger = LogManager.getLogger(WebDriverFactory.class);
+    private Logger logger = LogManager.getLogger(WebDriverFactory.class);
     private RemoteWebDriver driver;
 
     @Step("Получение типа драйвера")
@@ -41,31 +41,14 @@ public class WebDriverFactory {
     }
 
     @Step("Настройка удаленного драйвера")
-    public void setupRemoteDriver() {
-        logger.info("setup local driver");
+    private void setupRemoteDriver() {
+        logger.info("setup remote driver");
         ChromeOptions chromeOptions = new ChromeOptions();
         DesiredCapabilities capabilities = new DesiredCapabilities();
-        //DesiredCapabilities capabilities = DesiredCapabilities.chrome(); //в чем разница?
-
-
         chromeOptions.addArguments("--incognito");
-        chromeOptions.setExperimentalOption("excludeSwitches", Collections.singletonList("enable-automation"));//оставить
-        // или chromeOptions.addArguments("enable-automation");
         chromeOptions.addArguments("--disable-notifications");
         chromeOptions.addArguments("--disable-extensions");
         chromeOptions.addArguments("--dns-prefetch-disable");
-        chromeOptions.addArguments("--disable-gpu");
-        chromeOptions.addArguments("--no-sandbox");
-        chromeOptions.addArguments("--dns-prefetch-disable");
-        chromeOptions.addArguments("--ignore-certificate-errors");
-        chromeOptions.addArguments("--disabled-popup-blocking");
-        //chromeOptions.addArguments("--headless");
-        chromeOptions.setPageLoadStrategy(PageLoadStrategy.NORMAL);
-
-        capabilities.setCapability("enableVNC", true);
-        capabilities.setCapability("enableVideo", false);
-
-        //  chromeOptions.setPageLoadStrategy(PageLoadStrategy.eager);
         Map<String, Object> prefs = new HashMap<String, Object>();
         Map<String, Object> profile = new HashMap<String, Object>();
         prefs.put("googlegeolocationaccess.enabled", true);
@@ -73,14 +56,18 @@ public class WebDriverFactory {
         prefs.put("profile.default_content_setting_values.notifications", 1);
         prefs.put("profile.managed_default_content_settings", 1);
         chromeOptions.setExperimentalOption("prefs", prefs);
+        chromeOptions.setPageLoadStrategy(PageLoadStrategy.NORMAL);
+        capabilities.setCapability("enableVNC", true);
+        capabilities.setCapability("enableVideo", false);
 
-        // System.out.println(nameOfPackage + " " + nameOfClass);
+
         if (nameOfPackage.contains("mobile")) {
-            WebDriverManager.chromedriver().setup();
             Map<String, String> mobileEmulation = new HashMap<>();
             mobileEmulation.put("deviceName", "iPhone X");
             chromeOptions.setExperimentalOption("mobileEmulation", mobileEmulation);
+            capabilities.setCapability(ChromeOptions.CAPABILITY, chromeOptions);
         }
+
 
         capabilities.setCapability(ChromeOptions.CAPABILITY, chromeOptions);
         String driverURL = System.getProperty("driverurl");
@@ -131,7 +118,7 @@ public class WebDriverFactory {
         prefs.put("profile.managed_default_content_settings", 1);
         chromeOptions.setExperimentalOption("prefs", prefs);
 
-        // System.out.println(nameOfPackage + " " + nameOfClass);
+
         if (nameOfPackage.contains("mobile")) {
             WebDriverManager.chromedriver().setup();
             Map<String, String> mobileEmulation = new HashMap<>();
@@ -147,12 +134,13 @@ public class WebDriverFactory {
         logger.info("ЗАПУЩЕН ЛОКАЛЬНЫЙ ДРАЙВЕР");
     }
 
+
     @Step("Конфигурация драйвера")
     private void configureDriver() {
         driver.manage().deleteAllCookies();
         driver.manage().window().maximize();
-        driver.manage().timeouts().implicitlyWait(25, TimeUnit.SECONDS);
-        driver.manage().timeouts().pageLoadTimeout(25, TimeUnit.SECONDS);
+        driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+        driver.manage().timeouts().pageLoadTimeout(20, TimeUnit.SECONDS);
     }
 
 
