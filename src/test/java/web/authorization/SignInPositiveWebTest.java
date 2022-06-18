a504c8eede025351836e79d9c0715191b5e215fe
Михаил <mmamchenkov@fil-it.ru>
2022-06-18 20:50:35 +0300

    Добавлены правки для стабильности тестов

--- src/test/java/web/authorization/SignInPositiveWebTest.java
+++ src/test/java/web/authorization/SignInPositiveWebTest.java
@@ -7,6 +7,8 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.fail;
+
 @Feature(value = "Web")
 @Story("Авторизация пользователя на сайте 36.6")
 @DisplayName("Авторизация пользователя")
