b93a7965d333ec68bd0cf86681e5e2bd247ecfaa
Михаил <mmamchenkov@fil-it.ru>
2022-06-22 23:39:20 +0300

    добавлены исправления в тесты

--- src/test/java/web/modules/CheckShoppingCartWebTest.java
+++ src/test/java/web/modules/CheckShoppingCartWebTest.java
@@ -55,9 +55,9 @@ public class CheckShoppingCartWebTest extends BaseSettingsWebTests {
     @DisplayName("Пользователь удаляет 1 из 2-х товаров в корзине")
     @Test
     public void deleteMaterialsFromCart() {
-        openUrl(propertiesManager.getProperty("baseurl") + "p/47497");
+        openUrl(propertiesManager.getProperty("baseurl") + "p/2105");
         productCardPage.clickAddCartButton();
-        openUrl(propertiesManager.getProperty("baseurl") + "p/33090");
+        openUrl(propertiesManager.getProperty("baseurl") + "p/44226");
         productCardPage.clickAddCartButton();
         openUrl(propertiesManager.getProperty("baseurl") + "cart");
         cartPage.clickDeleteMaterials();
@@ -68,9 +68,9 @@ public class CheckShoppingCartWebTest extends BaseSettingsWebTests {
     @DisplayName("Пользователь удаляет все товары в корзине")
     @Test
     public void deleteAllMaterialsFromCart() {
-        openUrl(propertiesManager.getProperty("baseurl") + "p/47497");
+        openUrl(propertiesManager.getProperty("baseurl") + "p/2105");
         productCardPage.clickAddCartButton();
-        openUrl(propertiesManager.getProperty("baseurl") + "p/33090");
+        openUrl(propertiesManager.getProperty("baseurl") + "p/44226");
         productCardPage.clickAddCartButton();
         openUrl(propertiesManager.getProperty("baseurl") + "cart");
         cartPage.deleteAllMaterials();
@@ -81,7 +81,7 @@ public class CheckShoppingCartWebTest extends BaseSettingsWebTests {
     @DisplayName("Пользователь добавляет в корзину 2 товара и проверяет общую сумму")
     @Test
     public void checkTotalAmountProducts() {
-        openUrl(propertiesManager.getProperty("baseurl") + "p/47497");
+        openUrl(propertiesManager.getProperty("baseurl") + "p/2105");
         productCardPage.clickAddCartButton();
         int firstPrice = productCardPage.getProductPrice();
         openUrl(propertiesManager.getProperty("baseurl") + "p/44226");
@@ -95,7 +95,7 @@ public class CheckShoppingCartWebTest extends BaseSettingsWebTests {
     @DisplayName("Пользователь проверяет нотификацию после удаления товара")
     @Test
     public void checkNotificationCart() {
-        openUrl(propertiesManager.getProperty("baseurl") + "p/47497");
+        openUrl(propertiesManager.getProperty("baseurl") + "p/2105");
         productCardPage.clickAddCartButton();
         int firstPrice = productCardPage.getProductPrice();
         openUrl(propertiesManager.getProperty("baseurl") + "p/44226");
